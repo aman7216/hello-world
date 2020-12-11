bf21d6eba3b6dab1265050f79fd5a41fe1b402a2
aman7216 <75829845+aman7216@users.noreply.github.com>
2020-12-11 13:10:21 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,31 @@
+class p2
+{
+    void main(int n)
+    {   
+    boolean checkNumberPalindrome(int n){
+    int ulta_N = reverse(n);
+        if(n == ulta_N){
+            // System.out.println("It is a palindrome");
+            return true;
+        }
+        else{
+            // System.out.println("It is not a palindrome");
+            return false;
+        }
+        
+    }
+    
+    int reverse(int n)
+    {
+        // n = 345
+        int a,b,c;
+        int lastDigit = n%10;
+        int RemainingNumber = n/10; 
+        
+        int La = lastDigit*100+RemainingNumber;
+        int 
+       System.out.println(c);
+        // anw: 543
+    }
+
+        
